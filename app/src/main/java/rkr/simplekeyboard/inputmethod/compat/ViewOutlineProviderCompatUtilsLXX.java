4ee9885d4bad3c58155cdc02fa99ac6d77b68c10
HanzoDev1375 <ninjacodermain1375@gmail.com>
2024-09-25 16:45:10 +0330

    using new theme

--- app/src/main/java/rkr/simplekeyboard/inputmethod/compat/ViewOutlineProviderCompatUtilsLXX.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/compat/ViewOutlineProviderCompatUtilsLXX.java
@@ -27,46 +27,44 @@ import rkr.simplekeyboard.inputmethod.compat.ViewOutlineProviderCompatUtils.Inse
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
 class ViewOutlineProviderCompatUtilsLXX {
-    private ViewOutlineProviderCompatUtilsLXX() {
-        // This utility class is not publicly instantiable.
+  private ViewOutlineProviderCompatUtilsLXX() {
+    // This utility class is not publicly instantiable.
+  }
+
+  static InsetsUpdater setInsetsOutlineProvider(final View view) {
+    final InsetsOutlineProvider provider = new InsetsOutlineProvider(view);
+    view.setOutlineProvider(provider);
+    return provider;
+  }
+
+  private static class InsetsOutlineProvider extends ViewOutlineProvider implements InsetsUpdater {
+    private final View mView;
+    private static final int NO_DATA = -1;
+    private int mLastVisibleTopInsets = NO_DATA;
+
+    public InsetsOutlineProvider(final View view) {
+      mView = view;
+      view.setOutlineProvider(this);
     }
 
-    static InsetsUpdater setInsetsOutlineProvider(final View view) {
-        final InsetsOutlineProvider provider = new InsetsOutlineProvider(view);
-        view.setOutlineProvider(provider);
-        return provider;
+    @Override
+    public void setInsets(final InputMethodService.Insets insets) {
+      final int visibleTopInsets = insets.visibleTopInsets;
+      if (mLastVisibleTopInsets != visibleTopInsets) {
+        mLastVisibleTopInsets = visibleTopInsets;
+        mView.invalidateOutline();
+      }
     }
 
-    private static class InsetsOutlineProvider extends ViewOutlineProvider
-            implements InsetsUpdater {
-        private final View mView;
-        private static final int NO_DATA = -1;
-        private int mLastVisibleTopInsets = NO_DATA;
-
-        public InsetsOutlineProvider(final View view) {
-            mView = view;
-            view.setOutlineProvider(this);
-        }
-
-        @Override
-        public void setInsets(final InputMethodService.Insets insets) {
-            final int visibleTopInsets = insets.visibleTopInsets;
-            if (mLastVisibleTopInsets != visibleTopInsets) {
-                mLastVisibleTopInsets = visibleTopInsets;
-                mView.invalidateOutline();
-            }
-        }
-
-        @Override
-        public void getOutline(final View view, final Outline outline) {
-            if (mLastVisibleTopInsets == NO_DATA) {
-                // Call default implementation.
-                ViewOutlineProvider.BACKGROUND.getOutline(view, outline);
-                return;
-            }
-            // TODO: Revisit this when floating/resize keyboard is supported.
-            outline.setRect(
-                    view.getLeft(), mLastVisibleTopInsets, view.getRight(), view.getBottom());
-        }
+    @Override
+    public void getOutline(final View view, final Outline outline) {
+      if (mLastVisibleTopInsets == NO_DATA) {
+        // Call default implementation.
+        ViewOutlineProvider.BACKGROUND.getOutline(view, outline);
+        return;
+      }
+      // TODO: Revisit this when floating/resize keyboard is supported.
+      outline.setRect(view.getLeft(), mLastVisibleTopInsets, view.getRight(), view.getBottom());
     }
+  }
 }
